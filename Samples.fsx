011d678afa6263e4798868991f81bb54159c4da3
Eric Tobin <eatobin@gmail.com>
2022-08-23 17:34:25 -0500

    bump deps and playing around

---
+++ Samples.fsx
@@ -0,0 +1,13 @@
+let getOddSquares xs =
+    xs
+    |> List.filter (fun x -> x % 2 <> 0)
+    |> List.map (fun x -> x * x)
+
+printfn $"%A{getOddSquares [ 1..10 ]}"
+
+let getOddSquares2 xs =
+    xs
+    |> List.filter (fun x -> x % 2 <> 0)
+    |> List.map (fun x -> x * x)
+
+printfn $"%A{getOddSquares2 [ 100..110 ]}"
---
+++ print-name.fsx
@@ -0,0 +1,4 @@
+// [eric@linux-epth unit-testing-two](master)$ dotnet fsi print-name.fsx
+
+let name = "Devon"
+printfn $"Name: %s{name}"
